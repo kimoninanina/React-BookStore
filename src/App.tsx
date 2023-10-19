a62ce4dcc88f06f206004d1706953048ffaf5bb0
kimoninanina <nina02929@gmail.com>
2023-10-19 10:34:55 -0700

    Add Manage Loan for book checkout

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import { BookCheckoutPage } from "./layouts/BookCheckoutPage.tsx/BookCheckoutPage";
 import { oktaConfig } from "./lib/oktaConfig";
 import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
-import { LoginCallback, Security } from "@okta/okta-react";
+import { LoginCallback, SecureRoute, Security } from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
 import { ReviewListPage } from "./layouts/BookCheckoutPage.tsx/ReviewListPage/ReviewListPage";
+import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -58,6 +59,7 @@ export const App = () => {
               render={() => <LoginWidget config={oktaConfig} />}
             />
             <Route path="/login/callback" component={LoginCallback} />
+            <SecureRoute path='/shelf'> <ShelfPage/> </SecureRoute>
           </Switch>
         </div>
         <Footer />
--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -40,6 +40,13 @@ export const Navbar = () => {
                 Search Books
               </NavLink>
             </li>
+            {authState.isAuthenticated && (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/shelf">
+                  Shelf
+                </NavLink>
+              </li>
+            )}
           </ul>
           <ul className="navbar-nav ms-auto">
             {!authState.isAuthenticated ? (
