15fa60c89fc6f460eb997c4f622170a104e4cf1d
kimoninanina <nina02929@gmail.com>
2023-10-19 14:33:14 -0700

    Add Library services

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { LoginCallback, SecureRoute, Security } from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
 import { ReviewListPage } from "./layouts/BookCheckoutPage.tsx/ReviewListPage/ReviewListPage";
 import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
+import { MessagesPage } from "./layouts/MessagesPage/MessagesPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -60,6 +61,7 @@ export const App = () => {
             />
             <Route path="/login/callback" component={LoginCallback} />
             <SecureRoute path='/shelf'> <ShelfPage/> </SecureRoute>
+            <SecureRoute path='/messages'> <MessagesPage/> </SecureRoute>
           </Switch>
         </div>
         <Footer />
