5e006623a35f09bed0ec0348fee6e2b7b322d849
kimoninanina <nina02929@gmail.com>
2023-10-19 17:10:17 -0700

    Add admin

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import LoginWidget from "./Auth/LoginWidget";
 import { ReviewListPage } from "./layouts/BookCheckoutPage.tsx/ReviewListPage/ReviewListPage";
 import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
 import { MessagesPage } from "./layouts/MessagesPage/MessagesPage";
+import { ManageLibraryPage } from "./layouts/ManageLibraryPage/ManageLibraryPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -62,6 +63,7 @@ export const App = () => {
             <Route path="/login/callback" component={LoginCallback} />
             <SecureRoute path='/shelf'> <ShelfPage/> </SecureRoute>
             <SecureRoute path='/messages'> <MessagesPage/> </SecureRoute>
+            <SecureRoute path='/admin'> <ManageLibraryPage/> </SecureRoute>
           </Switch>
         </div>
         <Footer />
